3afb911489f1255326211918485b3b1296826173
RenePunch <repa7234@colorado.edu>
2025-05-01 21:14:39 +0000

    potential setup fix

--- src/challenges/setup.py
+++ src/challenges/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools','pyzbar'],
     zip_safe=True,
     maintainer='user',
     maintainer_email='user@todo.todo',
