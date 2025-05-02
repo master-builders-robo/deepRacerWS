118c69c2d2aa0355bacf3adf798e13964d7c43e6
master_builders <ddjjpp331@gmail.com>
2025-05-02 11:46:13 -0600

    basic parking done - Dalton

--- src/challenges/challenges/parallel_parking_two.py
+++ src/challenges/challenges/parallel_parking_two.py
@@ -25,6 +25,12 @@ BIG_WALL_LEN = 0.762     # 30 inches
 MIN_GAP = 0.15
 TURN_RATE = 10.0
 
+SHIMMY_RIGHT_TIME = 0.1
+SHIMMY_LEFT_TIME = 0.1
+SHIMMY_FORWARD_TIME = 0.1
+
+SHIMMY_DONE_DIST = 0.1
+
 class ParkingState(Enum):
     FIND_START     = auto()
     FIND_END       = auto()
@@ -45,26 +51,54 @@ class SpotDetectionAndParking(Node):
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
 
         self.scan_sub = self.create_subscription(LaserScan, 'scan', self.scan_callback, 10)
-        self.timer = self.create_timer(0.05, self.state_manager)
 
         self.state: ParkingState = ParkingState.FIND_START
 
         self.lidar = Lidar()
-        self.front_range = math.nan
-        self.back_range  = math.nan
-        self.left_range  = math.nan
-        self.right_range = math.nan
+        self.front_dist = math.nan
+        self.back_dist  = math.nan
+        self.left_dist  = math.nan
+        self.right_dist = math.nan
 
-        self.prev_time   = time.time()
+        self.prev_front_dist = self.front_dist
+        self.prev_back_dist  = self.back_dist
+        self.prev_left_dist  = self.left_dist
+        self.prev_right_dist = self.right_dist
+
+        self.delta_front_dist = self.front_dist - self.prev_front_dist
+        self.delta_back_dist  = self.back_dist  - self.prev_back_dist 
+        self.delta_left_dist  = self.left_dist  - self.prev_left_dist 
+        self.delta_right_dist = self.right_dist - self.prev_right_dist
+
+        self.start_time = time.time()
 
     def scan_callback(self, msg: LaserScan):
         self.lidar.update(msg)
-        self.front_range = self.lidar.get_dist(LidarAngles.FRONT_E.value)
-        self.back_range = self.lidar.get_dist(LidarAngles.BACK_E.value)
-        self.left_range = self.lidar.get_dist(LidarAngles.LEFT_E.value)
-        self.right_range = self.lidar.get_dist(LidarAngles.RIGHT_E.value)
 
-    def state_manager(self):
+        # Get previous results
+        self.prev_front_dist = self.front_dist  
+        self.prev_back_dist  = self.back_dist   
+        self.prev_left_dist  = self.left_dist   
+        self.prev_right_dist = self.right_dist  
+
+        # Get current results
+        self.front_dist = self.lidar.get_dist(LidarAngles.FRONT_E.value)
+        self.back_dist  = self.lidar.get_dist(LidarAngles.BACK_E.value)
+        self.left_dist  = self.lidar.get_dist(LidarAngles.LEFT_E.value)
+        self.right_dist = self.lidar.get_dist(LidarAngles.RIGHT_E.value)
+
+        # Get delta results
+        self.delta_front_dist = self.front_dist - self.prev_front_dist
+        self.delta_back_dist  = self.back_dist  - self.prev_back_dist 
+        self.delta_left_dist  = self.left_dist  - self.prev_left_dist 
+        self.delta_right_dist = self.right_dist - self.prev_right_dist
+
+        # Manage state
+        self.state_update()
+
+    def state_update(self):
+        self.get_logger().info(f'[state_update] {self.state}')
+
         if self.state == ParkingState.FIND_START:
             self.find_start()
         elif self.state == ParkingState.FIND_END:
@@ -81,17 +115,46 @@ class SpotDetectionAndParking(Node):
             self.done()
 
     def find_start(self):
+        if abs(self.delta_right_dist) >= SMALL_WALL_LEN / 2:
+            self.state = ParkingState.FIND_END
+            self.state_update()
+            return
         self.move(SLOW_SPEED, 0.0)
     def find_end(self):
+        if abs(self.delta_right_dist) >= SMALL_WALL_LEN / 2:
+            self.state = ParkingState.SHIMMY_RIGHT
+            self.state_update()
+            return
         self.move(SLOW_SPEED, 0.0)
+
     def shimmy_right(self):
-        pass
+        if time.time() - self.start_time <= SHIMMY_RIGHT_TIME:
+            self.state = ParkingState.SHIMMY_LEFT
+            self.state_update()
+            return
+        self.move(-SLOW_SPEED, TURN_RATE)
+
     def shimmy_left(self):
-        pass
+        if time.time() - self.start_time <= SHIMMY_RIGHT_TIME:
+            self.state = ParkingState.SHIMMY_FORWARD
+            self.state_update()
+            return
+        self.move(-SLOW_SPEED, -TURN_RATE)
+        
     def shimmy_forward(self):
-        pass
+        if self.right_dist <= SHIMMY_DONE_DIST:
+            self.state = ParkingState.DONE
+            self.state_update()
+            return
+        if time.time() - self.start_time <= SHIMMY_RIGHT_TIME:
+            self.state = ParkingState.SHIMMY_RIGHT
+            self.state_update()
+            return
+        self.move(SLOW_SPEED, 0.0)
+
     def straighten_out(self):
         pass
+    
     def done(self):
         self.move(0.0, 0.0)
 
