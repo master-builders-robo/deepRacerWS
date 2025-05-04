a026de2401512a932174a80893e68291a2f3eb6c
Chloe de Grivel <Chloe.WolffDegrivelperrigny@colorado.edu>
2025-05-04 13:54:00 -0600

    initial code for target_finding, see ig the robot can detect gree, going aroud the course implementation has not been added yet sig.Chloe

---
+++ src/deep_racer_launch/launch/target_finding.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Camera node
+    camera = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        name='camera'
+    )
+    # Hardware node
+    hardware = Node(
+        package='ros2_pca9685',
+        executable='listener',
+        name='hardware_interface'
+    )
+    # Target finder node
+    target_finder = Node(
+        package='racer',
+        executable='target_finder',  # This should match the name in your setup.py
+        name='target_finder',
+        output='screen'
+)
+    target_finder = Node(
+        package='racer',
+        executable='racer',
+        name='racer'
+    )
+    return LaunchDescription([
+        camera, hardware, target_finder
+    ])
